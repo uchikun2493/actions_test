7f12a5f8b85f8c0d8ba054175b1277cf17ab4319
uchikun2493 <um2493@gmail.com>
2020-06-24 15:16:39 +0900

    test upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # actions_test
+
+## Overview
+
+CI的なことがしたいんだ
+
+> ## Requirement
+> ## Install
+> ## Usage
+> ## Other
---
+++ scripts/test.py
@@ -0,0 +1,11 @@
+# my default import modules
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print('test')
+    
+if __name__ == '__main__':
+    main()
